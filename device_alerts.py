9acae44f3d7659db9d7c8c8123fbc73585d30b4e
jahidali25499 <67060137+jahidali25499@users.noreply.github.com>
2020-06-17 23:14:35 +0100

    Update device_alerts.py

--- device_alerts.py
+++ device_alerts.py
@@ -5,6 +5,11 @@ import operator
 
 # SELECT id FROM Properties WHERE devicename = '<device_name>' AND object_name = '<object_name:object_number>' AND <property> <expression> <value>
 
+# This script can be used to generate alerts based on personalized rules created in the device_rules.json file 
+# Rules currently detect whether certain values are higher, lower or equal to a certain value or string 
+# TO-DO - edit to include '>=" and "<=" symbols for values
+
+
 class Device_Alerts:
 
 	def __init__(self, input_file):
@@ -15,7 +20,8 @@ class Device_Alerts:
 		with open(str(input_file)) as file:
 			self.json_file = json.load(file)
 
-
+			
+	# Used in later function to convert numbers as strings to just numbers
 	def is_number(self, number):
 
 		try:
@@ -27,13 +33,15 @@ class Device_Alerts:
 		
 
 	def device_alerts_bacnet(self):
-
+		
+		# Convert symbols which do the same job
 		ineq_dict = {
 				"<": operator.lt, 
 				">": operator.gt, 
 				"=": operator.eq
 				}
-
+		
+		# Extract fields from 'device_rules.json' file 
 		for i in self.json_file:
 
 			device_name = i["device_name"]
@@ -45,15 +53,18 @@ class Device_Alerts:
 			search_value = i["search_value"]
 			search_unit = i["search_unit"]
 			alert = i["alert"]
-
+			
+			# Gather all values from chosen property within the search value and unit specified
 			chosen_property = '''SELECT * FROM Properties WHERE devicename = '{}' AND objectname = '{}:{}' 
 			AND time_stamp >= DATE_SUB(NOW(), INTERVAL {} {})'''.format(device_name, object_name, object_number, search_value, search_unit)
 
 			send_cmd = self.cursor.execute(chosen_property)
 			results = self.cursor.fetchall()
-
+			
+			# Compare to the values specified in the 'device_rules.json' file and trigger alert
 			for entry in results:
-
+				
+				# Change all numbers to floats or leave as strings
 				if self.is_number(entry[4]):
 					if ineq_dict[expression](float(entry[4]), value):
 						print(alert, entry[0])
