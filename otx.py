cc1bf37566b6702179a9573c6412eb5d2df2dd03
jahidali25499 <67060137+jahidali25499@users.noreply.github.com>
2020-06-17 23:00:31 +0100

    Update otx.py

--- otx.py
+++ otx.py
@@ -4,11 +4,12 @@ from sql_database import SQL_Database
 
 
 class OTX:
-
+	
+	# OTX API key needed in order to function
 	def __init__(self, otx_key):
 		self.otx = OTXv2(str(otx_key))
 
-
+	# Function to search the otx through single IOC (Indicator of Compromise) e.g. IP Addresss, File Hash, Domain  
 	def otx_search(self, indicator):
 
 		pulses = self.otx.search_pulses(str(indicator))
@@ -17,20 +18,22 @@ class OTX:
 		description = None
 
 		for i in range(len(pulses["results"])):
-
+			
+			# More fields can be selected - refer to otx_fields.txt 
 			pulse_id = pulses["results"][i]["id"]
 			name = pulses["results"][i]["name"]
 			description = pulses["results"][i]["description"]
-
+			
 			if pulse_id != None:
-
 				yield "Pulse ID: " + pulse_id + "\n" + "Name: " + name + "\n" + "Description: " + description + "\n"
-				
-
+		
+		
+	# Gather all pulses you have subscribed to and convert to json format
 	def otx_all(self):
 
 		all_pulses = self.otx.getall()
-
+		
+		# Data is inconveniently presented in pandas dataframe format - must be converted to json 
 		df = DataFrame(all_pulses)
 
 		json_format = df.to_json(orient="records")
